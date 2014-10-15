1394ddc628e33656d4ea14f8cf7e7c1d49056462
MHarris021 <mharris021@hotmail.com>
2014-10-15 12:20:32 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	build.gradle

---
+++ src/main/java/org/darcstarsolutions/finance/common/ISIN.java
@@ -0,0 +1,37 @@
+package org.darcstarsolutions.finance.common;
+
+import java.io.Serializable;
+
+/**
+ * Created by tetn on 10/15/14.
+ */
+public class ISIN implements Serializable{
+
+
+
+    private final String value;
+
+    public ISIN(String value){
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ISIN)) return false;
+
+        ISIN isin = (ISIN) o;
+
+        return value.equals(isin.value);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
